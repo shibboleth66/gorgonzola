98d251fea33ef276f51fe834afd0950f7eff74a2
Benjamin Gallagher <shibboleth@me.com>
2019-07-16 18:32:15 +0100

    Cleanup

--- gorgonzola/__init__.py
+++ gorgonzola/__init__.py
@@ -1,8 +1,9 @@
 from .aws_boto_session import BotoSession
 from .aws_sts_credentials import StsCredentials
 from .aws_lambda_function import LambdaFunction
-from .aws_s3_bucket import Bucket
-from .aws_iam_role import Role
+# from .aws_s3_bucket import Bucket
+# from .aws_iam_role import Role
 from .aws_ssm_globals import SsmGlobals
+from .aws_organizations import Organizations
 
 name = 'gorgonzola'
