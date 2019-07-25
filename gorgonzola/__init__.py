a3a1f3d14bd2032b9e167302bee2948b50fd8beb
Benjamin Gallagher <shibboleth@me.com>
2019-07-25 12:27:02 +0100

    Auto load classes

--- gorgonzola/__init__.py
+++ gorgonzola/__init__.py
@@ -1,9 +1,10 @@
 from .aws_boto_session import BotoSession
-from .aws_sts_credentials import StsCredentials
+from .aws_sts_credentials import Credentials
 from .aws_lambda_function import LambdaFunction
-# from .aws_s3_bucket import Bucket
-# from .aws_iam_role import Role
-from .aws_ssm_globals import SsmGlobals
+from .aws_s3_buckets import Buckets
+from .aws_iam_roles import Roles
+from .aws_ssm_regions import Regions
 from .aws_organizations import Organizations
+from .aws_dynamodb import DynamodbTable
 
 name = 'gorgonzola'
