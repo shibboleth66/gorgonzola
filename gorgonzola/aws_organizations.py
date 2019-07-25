aaadad3e5e1fa2409e5bdba3e6c1f4e7741f3839
Benjamin Gallagher <shibboleth@me.com>
2019-07-25 12:26:01 +0100

    Improved get methods

--- gorgonzola/aws_organizations.py
+++ gorgonzola/aws_organizations.py
@@ -2,44 +2,55 @@ from .aws_boto_session import BotoSession
 
 
 class Organizations(BotoSession):
-    """
+    """List AWS Organizations Accounts
 
+    Generate full list of Accunts present within an AWS Organization.
+
+    **kwargs:
+        RoleArn (string, required): ARN of AWS IAM Role
+            Formatted ARN of IAM Role.
+
+    Examples:
+        orgs = gorgonzola.Organizations(
+            'RoleArn': 'arn:aws:iam::123456789012:role/OrganizationAccountAccessRole'
+        )
+
+        Return list of dictionaries, each containing Account Information.
+        orgs.get_accounts()
+
+        Return list of Account IDs.
+        orgs.get_accounts_list()
     """
 
     # ==========================================================
+    # Initialize Organizations object
     def __init__(self, **kwargs):
 
         # Global Parameters.
         self.service_name = 'organizations'
         self.service_interface = 'client'
-        self.account_info = []
-        self.account_list = []
 
         # Required Parameters.
         self.role_arn = kwargs.get('RoleArn', None)
-
-        # Optional Parameters (with defaults)
-        self.region_name = kwargs.get('RegionName', 'eu-west-2')
 
         # Set parameters for super init.
         super_params = {
             'ServiceInterface': self.service_interface,
             'ServiceName': self.service_name,
-            'RoleArn': self.role_arn,
-            'RegionName': self.region_name
+            'RoleArn': self.role_arn
         }
 
         # Initialise parent object.
         super().__init__(**super_params)
 
     # ==========================================================
-    def __get_accounts(self):
+    # Generate list of ALL accounts
+    def __query_all_accounts(self):
+        self.info = []
 
-        # Query SSM for region info.
         resp = self.boto.list_accounts()
 
-        # Add response 'Accounts' to info list.
-        self.account_info.extend(resp['Accounts'])
+        self.info.extend(resp['Accounts'])
 
         # Continue to query until no NextToken present.
         while 'NextToken' in resp:
@@ -47,24 +58,33 @@ class Organizations(BotoSession):
                 NextToken=resp['NextToken']
             )
 
-            # Add response 'Accounts' to info list.
-            self.account_info.extend(resp['Accounts'])
+            self.info.extend(resp['Accounts'])
 
     # ==========================================================
-    def get_account_info(self):
+    # Return account info (list of dicts)
+    def get_accounts(self):
+        self.__query_all_accounts()
 
-        # Generate list of region info.
-        self.__get_accounts()
+        return self.info
 
-        # Return region info (list of dicts)
-        return self.account_info
+    # ==========================================================
+    # Return account list
+    def get_accounts_list(self):
+        self.list = []
+        
+        for i in self.get_accounts():
+            self.list.append(
+                i.get('Id', None)
+            )
 
-        # ==========================================================
-    def get_account_list(self):
+        return self.list
 
-        # Extract account id from generated info.
-        for account in self.get_account_info():
-            self.account_list.append(account.get('Id', None))
+    # ==========================================================
+    # def get_ou(self, account):
 
-        # Return account list.
-        return self.account_list
+    #     resp = self.boto.list_parents(
+    #         ChildId=account
+    #     )
+
+    #     if len(resp.get('Parents', [])) != 0:
+    #         return resp['Parents'][0]
