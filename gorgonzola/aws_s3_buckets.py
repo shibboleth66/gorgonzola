8d6ee26f172dde3b9163920080a4ec1829f9eb7e
Benjamin Gallagher <shibboleth@me.com>
2019-07-30 15:36:45 +0100

    Better documentation and method naming

--- gorgonzola/aws_s3_buckets.py
+++ gorgonzola/aws_s3_buckets.py
@@ -1,7 +1,23 @@
 from .aws_boto_session import BotoSession
 
 
-class Buckets(BotoSession):
+class S3Buckets(BotoSession):
+    """List any/all S3 buckets in account.
+
+    Generate list of buckets found in account.
+
+    Account ID is specified in ARN of role passed as argument.
+
+    Parameters
+    ----------
+    RoleArn : str
+        ARN of AWS IAM Role.
+
+    Methods
+    ----------
+    get_buckets(DetailLevel='low')
+        Returns list of S3 Buckets
+    """
 
     # ==========================================================
     # Initialize Roles object.
@@ -40,7 +56,7 @@ class Buckets(BotoSession):
 
     # ==========================================================
     # Return low detail level
-    def get_detail_low(self):
+    def _get_detail_low(self):
         for i in self.info:
             self.list.append(
                 i.get('Name', None)
@@ -50,7 +66,15 @@ class Buckets(BotoSession):
 
     # ==========================================================
     # Get info
-    def get_info(self, DetailLevel='low'):
+    def get_buckets(self, DetailLevel='low'):
+        """Generates list of buckets
+
+        Parameters
+        ----------
+        DetailLevel : str, optional
+            Required detail of results (Default is 'low')
+            If DetailLevel is 'high' returns list of dictionaries
+        """
 
         # Auto-generate data.
         self._query_s3_for_bucket_data()
@@ -59,4 +83,4 @@ class Buckets(BotoSession):
         if DetailLevel.lower() == 'high':
             return self.info
         else:
-            return self.get_detail_low()
+            return self._get_detail_low()
