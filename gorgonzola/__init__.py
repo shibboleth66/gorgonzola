57442cb897b43aa3942971acfb7647ee76852cbe
Benjamin Gallagher <shibboleth@me.com>
2019-07-30 15:35:11 +0100

    Added description and better named imports

--- gorgonzola/__init__.py
+++ gorgonzola/__init__.py
@@ -1,10 +1,11 @@
 from .aws_boto_session import BotoSession
-from .aws_sts_credentials import Credentials
+from .aws_sts_credentials import STSCredentials
 from .aws_lambda_function import LambdaFunction
-from .aws_s3_buckets import Buckets
-from .aws_iam_roles import Roles
-from .aws_ssm_regions import Regions
+from .aws_s3_buckets import S3Buckets
+from .aws_iam_roles import IAMRoles
+from .aws_ssm_global_parameters import SSMGlobalParameters
 from .aws_organizations import Organizations
 from .aws_dynamodb import DynamodbTable
 
 name = 'gorgonzola'
+description = 'Helper functions for AWS Multi-Account Management'
