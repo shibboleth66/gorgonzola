24d11817ff5ebc4d69705fd96ae5ff90f5027fb1
Ben Gallagher <shibboleth@me.com>
2020-08-28 17:26:41 +0100

    Autoload new modules

--- gorgonzola/__init__.py
+++ gorgonzola/__init__.py
@@ -3,6 +3,8 @@ from .aws_sts_credentials import STSCredentials
 from .aws_lambda_function import LambdaFunction
 from .aws_s3_buckets import S3Buckets
 from .aws_iam_roles import IAMRoles
+from .aws_iam_alias import IAMAccountAlias
+from .aws_iam_idp import IAMIdentityProvider
 from .aws_ssm_global_parameters import SSMGlobalParameters
 from .aws_organizations import Organizations
 from .aws_dynamodb_table import DynamodbTable
